dfa14ce7e08b33897fc077a4a81f5adf9bc8d421
maayanchok <71598724+maayanchok@users.noreply.github.com>
2022-08-01 08:24:40 +0300

    Update models.py

--- simba/simbapp/models.py
+++ simba/simbapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from models import model
 # Create your models here.
+
+class User(models.Model):
+  name = models.CharField(max_lenght = 50, default = None)
+  birthdate = models.Date()
+  email = models.EmailField()
+  gender = models.CharField(max_lenght = 124, default = None)
+  bio = models.CharField(max_lenght = 124, default = None)
+  
+  
+  
+  
+  
